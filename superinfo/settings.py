58316e6791394582a45646648d8b9228465f596b
Trong Hai <truongtronghai@users.noreply.github.com>
2022-07-30 15:29:16 +0700

    Update settings.py

--- superinfo/settings.py
+++ superinfo/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # gmail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'truongtronghai@gmail.com'
-EMAIL_HOST_PASSWORD = 'jvkabchjytowrpco'  # past the key or password app here
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''  # past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
